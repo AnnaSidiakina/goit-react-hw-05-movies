3b6ffccf18a07a89b55c254c3e38c11f7b202671
AnnaSidiakina <102148925+AnnaSidiakina@users.noreply.github.com>
2022-11-04 10:30:04 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { lazy } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import TrendingMoviesList from 'pages/Home/Home';
 // import { Movies } from '../pages/Movies/Movies';
 import Layout from './Layout/Layout';
